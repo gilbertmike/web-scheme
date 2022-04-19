a6e6470f4fd37f1f61c677453817feedcb220d6f
Michael Gilbert <gilbertm@mit.edu>
2022-04-19 16:41:00 -0400

    [reg-machine] More types.

--- reg-machine/types.h
+++ reg-machine/types.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <stdexcept>
+#include <string>
 #include <variant>
 
 #include "gc.h"
@@ -10,16 +12,29 @@ struct object_t;
 // variables contain pointer to an object
 typedef object_t* value_t;
 
+// unassigned variables
+extern object_t* unassgined;
+
 struct pair_t {
   value_t car;
   value_t cdr;
 };
 
-// unassigned variables
-extern object_t* unassgined;
+struct quoted_t {
+  std::string value;
+};
+
+struct label_t {
+  uintptr_t dst;
+};
 
 struct object_t : garbage_collected_t {
-  std::variant<int64_t, pair_t, bool> value;
+  std::variant<int64_t, bool, pair_t, quoted_t, label_t> value;
+
+  object_t() : value() {}
+
+  template <typename T>
+  object_t(T&& value) : value(std::forward(value)) {}
 
   void mark_children() {
     std::visit(
@@ -32,7 +47,12 @@ struct object_t : garbage_collected_t {
           } else if constexpr (std::is_same_v<T, pair_t>) {
             arg.car->mark_children();
             arg.cdr->mark_children();
+          } else if constexpr (std::is_same_v<T, quoted_t>) {
+            /* do nothing */
+          } else if constexpr (std::is_same_v<T, label_t>) {
+            /* do nothing */
           } else {
+            throw std::runtime_error("not all object variant handled");
           }
         },
         value);
