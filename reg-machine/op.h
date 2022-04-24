1785f5d927460cd68f17e620308ba92cd7ea7c5b
Michael Gilbert <gilbertm@mit.edu>
2022-04-24 10:35:23 -0400

    [reg-machine] buch of primitive opts that need support

--- reg-machine/op.h
+++ reg-machine/op.h
@@ -13,6 +13,19 @@ op_t* str_to_op(std::string_view op_str);
 extern op_t* add_op;
 extern op_t* sub_op;
 extern op_t* int_eq_op;
+
 extern op_t* cons_op;
 extern op_t* car_op;
 extern op_t* cdr_op;
+
+extern op_t* primitive_procedure_op;        // TODO
+extern op_t* apply_primitive_procedure_op;  // TODO
+
+extern op_t* primitive_procedure_test_op;  // TODO
+extern op_t* make_compiled_procedure_op;   // TODO
+extern op_t* compiled_procedure_env_op;    // TODO
+
+extern op_t* extend_environment_op;        // TODO
+extern op_t* lookup_variable_value_op;     // TODO
+extern op_t* compiled_procedure_entry_op;  // TODO
+extern op_t* define_variable_op;           // TODO
