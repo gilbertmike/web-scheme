c8379e786ca054b4330ec956b1432e4fb9f5e35c
Chengyuan Ma <macy404@mit.edu>
2022-04-25 18:37:40 -0400

    [reg-machine] Test varargs, looks good

--- reg-machine/main.cpp
+++ reg-machine/main.cpp
@@ -1,12 +1,13 @@
 #include <assert.h>
 
+#include <iostream>
 #include <fstream>
 
 #include "machine.h"
 #include "parser.h"
 #include "sicp.h"
 
-const std::string TEST_RMA_FILE = "fact.rma";
+const std::string TEST_RMA_FILE = "varargs.rma";
 
 void test_reg_machine() {
   std::ifstream test_f(TEST_RMA_FILE);
@@ -17,6 +18,11 @@ void test_reg_machine() {
   machine.start();
 
   assert(machine.pc.get().as<label_t>().dst == machine.instructions.size());
+  
+  for (auto& reg : machine.rfile) {
+    auto value = reg.get();
+    std::cout << reg.get_name() << ": " << value << std::endl;
+  }
 }
 
 int main(int argc, char* argv[]) {
